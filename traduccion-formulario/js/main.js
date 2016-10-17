18f6ad132cbe1df9506b8277d3409d84edbe4719
OlivaGL <ologavilo@gmail.com>
2016-10-17 12:12:58 -0500

    Terminé =) ejercicio 1

---
+++ traduccion-formulario/js/main.js
@@ -0,0 +1,5 @@
+var inicioFormulario = document.getElementById("form-signin-heading").innerHTML = ("Por favor inicia sesión");
+var correoElec = document.getElementById("inputEmail").placeholder = ("Correo Electrónico");
+var contraseña = document.getElementById("inputPassword").placeholder = ("Contraseña");
+var recordar = document.getElementsByTagName("span")[0].innerHTML = ("Recordar datos");
+var recordar = document.getElementsByTagName("button")[0].innerHTML = ("Iniciar sesión");
